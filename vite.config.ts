0a19ef744d796797a2eebead8cd5a3de2a8e776c
karlos123230 <karlosviolinn@gmail.com>
2024-12-27 15:00:17 -0300

    chore: ajusta configurações para deploy no Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,19 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      }
+    }
+  },
+  server: {
+    headers: {
+      'Content-Type': 'application/javascript'
+    }
+  }
 })
