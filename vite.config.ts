667c8f4c1f323bfe58bb3a0ff932401671569e2f
karlos <karlosviolinn@gmail.com>
2024-12-27 10:42:55 -0300

    fix: update vite config for build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') return;
+        warn(warning);
+      }
+    }
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
